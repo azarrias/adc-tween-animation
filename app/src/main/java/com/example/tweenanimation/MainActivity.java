5a2cbb39dc10c893f1663601b41e010ca71b9fa6
azarrias <adolfo.zarrias@gmail.com>
2019-04-07 11:07:51 +0200

    Add TweenAnimation example on a TextView

--- app/src/main/java/com/example/tweenanimation/MainActivity.java
+++ app/src/main/java/com/example/tweenanimation/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.tweenanimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_view);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        textView.startAnimation(animation);
     }
 }
